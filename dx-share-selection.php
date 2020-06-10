f2ba0c891d18cba2a76e36ee0725d5ee999c3703
Denislav Parvanov <denislav.parvanov@gmail.com>
2020-06-10 20:11:10 +0300

    Feature: Update plugin version
    Update plugin version and remove unused DXSS_AUTHOR
    
    https://app.asana.com/0/1165279318339835/1179573467864000/f

--- dx-share-selection.php
+++ dx-share-selection.php
@@ -11,8 +11,7 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-define('DXSS_VERSION', '1.2');
-define('DXSS_AUTHOR', 'Mario Peshev');
+define('DXSS_VERSION', '1.4');
 
 if (!defined('WP_CONTENT_URL')) {
 	$dxss_pluginpath = get_option('siteurl') . '/wp-content/plugins/' . plugin_basename(dirname(__FILE__)) . '/';
