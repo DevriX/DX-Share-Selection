3519fe3f018d9ae5a5a2eff8e54773e46b3e145e
Aleks7391 <aleks.ganev7@gmail.com>
2021-12-13 14:17:18 +0200

    Removed buttons from WP-Socializer that were not working.

--- src/class-dxss-wpsr.php
+++ src/class-dxss-wpsr.php
@@ -24,7 +24,7 @@ class DXSS_WPSR {
 			$i                     = 0;
 			foreach ( $wpsr_socialsites_list as $key => $value ) {
 				// Messenger needs fb-app-id.
-				if ( in_array( $key, array( 'addtofavorites', 'fbmessenger' ), true ) ) {
+				if ( in_array( $key, array( 'addtofavorites', 'fbmessenger', 'comments', 'phone', 'wechat' ), true ) ) {
 					continue;
 				}
 				if ( 0 !== intval( $i ) ) {
@@ -33,9 +33,9 @@ class DXSS_WPSR {
 					$temp_url = str_replace( '{title-plain}', '{title}', $temp_url );
 					$temp_url = str_replace( '{short-url}', '{surl}', $temp_url );
 					$temp_url = str_replace( '{twitter-username}', '', $temp_url );
-					echo '<li><a href="#" rel="' . esc_html( $temp_url ) . '">' . esc_html( $key ) . '</a></li>';
+					echo '<li><a href="#" rel="' . esc_html( $temp_url ) . '">' . esc_html( $value['name'] ) . '</a></li>';
 				}
-				$i ++;
+				$i++;
 			}
 		}
 	}
